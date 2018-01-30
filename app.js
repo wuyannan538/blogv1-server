18cbdeacdb2921f8d82a2f2bb9724e1364477c2f
wuyannan <wushuang1591@126.com>
2018-01-30 22:58:38 +0800

    init server

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/login', function (req, res) {
+    res.json({code: 200})
+})
+
+var server = app.listen(9090, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
